eb1ab4a60e7d7ff888aa260b809b1e2d05f23cdb
hantang <34468720+hantang@users.noreply.github.com>
2024-12-13 23:18:40 +0800

    Add debug param

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 
 	"github.com/hantang/smartedudlgo/internal/ui"
 )
 
 func main() {
-	// slog.SetLogLoggerLevel(slog.LevelDebug)
-	slog.Debug("Debug mode enabled")
+	debug := flag.Bool("debug", false, "Enable debug logging")
+	flag.Parse()
+	if *debug {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+		slog.Debug("Debug mode enabled")
+	}
 
 	ui.InitUI()
 }
